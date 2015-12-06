49774ab30af7d9b9d35174625c20224cfc0b9115
Oleksandr Malakhovskyi <zokzok95@gmail.com>
2015-12-06 16:30:09 +0100

    Added DBHelper

---
+++ Dream/app/src/main/java/com/example/oleksandr/dream/DB/DBHelper.java
@@ -0,0 +1,31 @@
+package com.example.oleksandr.dream.DB;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.support.ConnectionSource;
+
+/**
+ * Created by Oleksandr on 06.12.2015.
+ */
+public class DBHelper extends OrmLiteSqliteOpenHelper {
+
+    private static final String DATABASE_NAME = "dream.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        //TODO Add raw.ormlite_config
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database, ConnectionSource connectionSource) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+
+    }
+}
