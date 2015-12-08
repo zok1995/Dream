f42523bd4bcba597a0c2f2641a7ab913c4cf415f
Oleksandr Malakhovskyi <zokzok95@gmail.com>
2015-12-08 18:26:05 +0100

    From Main Actitvity to new Dream, all field from xml are synchronized to java code

--- Dream/app/src/main/java/com/example/oleksandr/dream/MainActivity.java
+++ Dream/app/src/main/java/com/example/oleksandr/dream/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.oleksandr.dream;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button mButtonNext;
+    private EditText mEditTextEnterDream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonNext = (Button) findViewById(R.id.dream_button);
+        mEditTextEnterDream = (EditText) findViewById(R.id.editTextEnterDream);
+
+        mButtonNext.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mButtonNext){
+            startActivity(new Intent(this, NewDream.class));
+        }
     }
 }
