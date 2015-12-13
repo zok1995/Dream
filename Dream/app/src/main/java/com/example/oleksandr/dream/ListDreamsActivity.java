d46614c1aa2cdcf5cc29e55fa64431687007738e
Oleksandr Malakhovskyi <zokzok95@gmail.com>
2015-12-13 23:05:02 +0100

    Started reading from DB, without adapter, inflater for list_view and activity lust_dreans are ready

--- Dream/app/src/main/java/com/example/oleksandr/dream/ListDreamsActivity.java
+++ Dream/app/src/main/java/com/example/oleksandr/dream/ListDreamsActivity.java
@@ -1,8 +1,11 @@
 package com.example.oleksandr.dream;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.oleksandr.dream.DB.DBHelper;
@@ -11,22 +14,34 @@ import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.dao.Dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class ListDreamsActivity extends AppCompatActivity {
     private DBHelper mDbHelper = null;
     private TextView mTextView;
+    private ListView mListView;
     private Dao<DreamDetails, Integer> dreamDetailsDao;
+    private List<DreamDetails> dreamList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_dreams);
-
+        mListView = (ListView) findViewById(R.id.listViewAllDreams);
         mTextView = (TextView) findViewById(R.id.textViewTryReadFromDB);
 
         try {
             dreamDetailsDao = getHelper().getDreamDetailsesDao();
-            Log.i("DATA FROM DB-----", "Data:" + dreamDetailsDao.queryForAll());
+            dreamList = dreamDetailsDao.queryForAll();
+
+            // Set the header of the ListView
+            final LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            final View view = inflater.inflate(R.layout.list_view, mListView, false);
+            mListView.addHeaderView(view);
+
+            // TODO  Create my own adapter!
+           // mListView.setAdapter(new Rec);
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
