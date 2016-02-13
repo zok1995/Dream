ffd3989895c463e93044e857f48154462f4fa552
OleksandrM95 <zokzok95@gmail.com>
2016-02-13 17:35:26 +0200

    New architecture, still working

--- Dream/app/src/main/java/com/example/oleksandr/dream/MainActivity.java
+++ Dream/app/src/main/java/com/example/oleksandr/dream/MainActivity.java
@@ -1,40 +1,121 @@
 package com.example.oleksandr.dream;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private Button mButtonNext;
-    private EditText mEditTextEnterDream;
+import com.example.oleksandr.dream.Adapters.AdapterArrayDream;
+import com.example.oleksandr.dream.DB.DBHelper;
+import com.example.oleksandr.dream.DB.DreamDetails;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.stmt.DeleteBuilder;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private DBHelper mDbHelper = null;
+    private DreamDetails dreamDetails;
+    private ListView mListView;
+    private Button mButton;
+    private Dao<DreamDetails, Integer> dreamDetailsDao;
+    private List<DreamDetails> dreamList;
+    private int selectedRecordPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.listViewAllDreams);
+        mButton = (Button) findViewById(R.id.buttonDelete);
+/*
+        final DreamDetails dreamDetails = new DreamDetails();
+        String txtDream = "PORNO";
+        dreamDetails.dreamName = txtDream;
+//            Log.i("TAAAAAAG", "onClick ");
+        try {
+            //Insert do DB
+            final Dao<DreamDetails, Integer> daoDream = getHelper().getDreamDetailsesDao();
+            daoDream.create(dreamDetails);
+            Log.i("TAAAAAAG", "onClick insert " + "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+*/
+        try {
+            dreamDetailsDao = getHelper().getDreamDetailsesDao();
+            dreamList = dreamDetailsDao.queryForAll();
 
-        mButtonNext = (Button) findViewById(R.id.dream_button);
-        mEditTextEnterDream = (EditText) findViewById(R.id.editTextEnterDream);
+            // Set the header of the ListView
+            final LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            final View view = inflater.inflate(R.layout.list_view, mListView, false);
+            mListView.addHeaderView(view);
 
-        mButtonNext.setOnClickListener(this);
+            // my own adapter!
+            mListView.setAdapter(new AdapterArrayDream(this,R.layout.list_view,dreamList,dreamDetailsDao));
+            mListView.setOnItemClickListener(this);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // This is how, DatabaseHelper can be initialized for future use
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if(i > 0)
+        {
+            selectedRecordPosition = i - 1;
+            // Details screen showing code can put over here
+            DeleteBuilder<DreamDetails, Integer> deleteBuilder = dreamDetailsDao.deleteBuilder();
+            //  deleteBuilder.where().eq("cMameDream", )
+            final Intent intent = new Intent(this, ViewDream.class);
+            //   intent.putExtra("Name",dreamList.get(selectedRecordPosition));
+            Log.i("TAAAAAAG", "onClick insert " + dreamList.get(selectedRecordPosition));
+            startActivity(intent);
+        }
     }
 
     @Override
-    public void onClick(View view) {
-        if (mEditTextEnterDream.getText().toString().matches("")){
-            Toast.makeText(MainActivity.this,"Type you Dream!",Toast.LENGTH_LONG).show();
-            }else {
-            if (view == mButtonNext){
-                startActivity(new Intent(this, NewDream.class));
-                Intent intent = new Intent(MainActivity.this, NewDream.class);
-                intent.putExtra("Dream", mEditTextEnterDream.getText().toString());
-                startActivity(intent);
-//                Log.i("TAAAAAAG", "ELSE " );
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list_dreams, menu);
+        return true;
+    }
+
+    /*  public void getId(int id){
+            try {
+
+
+               // Log.i("ID","   ififififififiififififi 111" + id1);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
+        } */
+    public void onClick(View view) throws SQLException {
+     /*   Where<DreamDetails, Integer> id1 = dreamDetailsDao.queryBuilder().where().eq("cMameDream", 1);
+        QueryBuilder<DreamDetails,Integer> queryBuilder = dreamDetailsDao.queryBuilder();
+        queryBuilder.where().eq(DreamDetails.ID_FIELD, "dream_id");
+        List<DreamDetails> list = queryBuilder.query();
+        Log.i("MEGA111", "               " + list);
+        DeleteBuilder<DreamDetails, Integer> deleteBuilder = dreamDetailsDao.deleteBuilder();*/
+        mDbHelper.deleteAllData();
+        finish();
+        startActivity(getIntent());
+    }
+    private DBHelper getHelper() {
+        if (mDbHelper == null) {
+            mDbHelper = OpenHelperManager.getHelper(this, DBHelper.class);
         }
+        return mDbHelper;
     }
 }
