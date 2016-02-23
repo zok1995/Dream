462c5a1a68f26d9ac6a3370e350db47beb111736
maxr96 <maxr96@gmail.com>
2016-02-23 19:42:24 +0100

    Small changes to variables in NewDream and white text for Name of programm

--- Dream/app/src/main/java/com/example/oleksandr/dream/MainActivity.java
+++ Dream/app/src/main/java/com/example/oleksandr/dream/MainActivity.java
@@ -102,6 +102,21 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         return true;
     }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
     private DBHelper getHelper() {
         if (mDbHelper == null) {
             mDbHelper = OpenHelperManager.getHelper(this, DBHelper.class);
