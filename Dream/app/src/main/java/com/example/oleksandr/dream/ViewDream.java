f96f22c6a5e4c254f0c110ca4968a144c3b56aeb
OleksandrM95 <zokzok95@gmail.com>
2016-02-24 12:27:50 +0200

    Starting whriting dream describtion

--- Dream/app/src/main/java/com/example/oleksandr/dream/ViewDream.java
+++ Dream/app/src/main/java/com/example/oleksandr/dream/ViewDream.java
@@ -53,18 +53,5 @@ public class ViewDream extends AppCompatActivity {
         }
         return mDbHelper;
     }
-    /*
-        final DreamDetails dreamDetails = new DreamDetails();
-        String txtDream = "Go";
-        dreamDetails.dreamName = txtDream;
-//            Log.i("TAAAAAAG", "onClick ");
-        try {
-            //Insert do DB
-            final Dao<DreamDetails, Integer> daoDream = getHelper().getDreamDetailsesDao();
-            daoDream.create(dreamDetails);
-            Log.i("TAAAAAAG", "onClick insert " + "");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-*/
+
 }
